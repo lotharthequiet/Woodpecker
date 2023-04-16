5f994e302f04bb2d7ac3b531a89f5719e73a6783
Lothar TheQuiet <104657079+lotharthequiet@users.noreply.github.com>
2023-04-16 12:42:07 -0500

    Updated to new scanning option with functions.

--- Pecker.py
+++ Pecker.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python3
-
-import socket
-import csv
-import sys
 import nmap
+import argparse
+import csv
+import socket
+import sys
+import dns.resolver
+import logging
+
+"""
+Logging Levels:
+------------------------------------------------------------
+DEBUG: Detailed info
+INFO: Confirmation of things working correctly
+WARNING: (Default level) Indication things are not so good
+ERROR: More serious prob preventing app from running
+CRITICAL: Serious error
+"""
 
 class Device:
     def __init__(self, ip, hostname):
@@ -14,62 +26,81 @@ class Device:
     def add_open_port(self, port):
         self.open_ports.append(port)
 
-# Create a new nmap scanner
-try:   
-    Pecker = nmap.PortScanner()
-except nmap.PortScannerError:
-    print('Nmap not found', sys.exc_info()[0])
-    sys.exit(1)
-except:
-    print("Unexpected error:", sys.exc_info()[0])
-    sys.exit(1)
+def scan_ports(ip):
+    try:
+        Pecker = nmap.PortScanner()
+    except nmap.PortScannerError:
+        print('Nmap not found', sys.exc_info()[0])
+        sys.exit(1)
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+        sys.exit(1)
+    Pecker.scan(ip, arguments='-sS')
+    return Pecker[ip]['tcp'].keys()
 
-#OsDetector = nmap.PortScanner()
+def scan_os(ip):
+    try:
+        Pecker = nmap.PortScanner()
+    except nmap.PortScannerError:
+        print('Nmap not found', sys.exc_info()[0])
+        sys.exit(1)
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+        sys.exit(1)
+    Pecker.scan(ip, arguments='-O')
+    return Pecker[ip]['osmatch'][0]['name']
+
+def resolve_hostname(ip, dns_server):
+    resolver = dns.resolver.Resolver(configure=False)
+    resolver.nameservers = [dns_server] 
+    return resolver.query(ip, "PTR")[0].to_text()
+
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = s.getsockname()[0]
+    s.close()
+    return ip
 
 def main():
-    # Define the target host IP address and ports to be scanned
-    if len(sys.argv) != 3:
-        print("Usage: Pecker.py network_address/subnet_mask ports")
-        return
-    target = sys.argv[1]
-    ports = sys.argv[2]
 
-    # Define the port scanner
-    Pecker.scan(target, ports, arguments='-O -sT -sU -sV')
+    PeckerLog = logging.getLogger(__name__)
+    PeckerLog.setLevel(logging.DEBUG)
+    PeckerFMT = logging.Formatter('%(created)f:%(levelname)s:%(message)s')
+    PeckerStream = logging.StreamHandler()
+    PeckerStream.setFormatter(PeckerFMT)
+    PeckerLog.addHandler(PeckerStream)
+    PeckerLog.debug('Erecting the Pecker Network Scanner.')
 
-    # Define the OS scanner
-    #OsDetector.scan(target, arguments='-O')
-    print('This network has been fondled by Pecker.')
-    for host in Pecker.all_hosts():
-        print('Host: %s (%s)' % (host, Pecker[host].hostname()))
-        print('State: %s' % Pecker[host].state())
+    local_ip = get_local_ip()
+    print(local_ip)
+    PeckerParser = argparse.ArgumentParser(description='Pecker - Network Scanner')
+    PeckerParser.add_argument('ips', type=str, help='IP address(es) to scan. (10.0.0.1 OR 10.0.0.0/24)', default=local_ip)
+    PeckerParser.add_argument('dns', type=str, help='DNS Server to use for name resolution. Default = 8.8.8.8', default='8.8.8.8')
+    PeckerParser.add_argument('output', type=str, help='Output filename. Default = scan.csv', default='scan.csv')
+    PeckerArgs = PeckerParser.parse_args()
 
-        if 'osmatch' in Pecker[host]:
-            for osmatch in Pecker[host]['osmatch']:
-                print('OsMatch.name : {0}'.format(osmatch['name']))
-                print('OsMatch.accuracy : {0}'.format(osmatch['accuracy']))
-                print('OsMatch.line : {0}'.format(osmatch['line']))
-                print('')
+    if PeckerArgs.ips == local_ip:
+        PeckerLog.debug('Scanning the local ip address: ')
+        PeckerLog.debug(local_ip)
+    else:
+        PeckerLog.debug('Scanning ip address(es): ')
+        PeckerLog.debug(PeckerArgs.ips)
 
-                if 'osclass' in osmatch:
-                    for osclass in osmatch['osclass']:
-                        print('OsClass.type : {0}'.format(osclass['type']))
-                        print('OsClass.vendor : {0}'.format(osclass['vendor']))
-                        print('OsClass.osfamily : {0}'.format(osclass['osfamily']))
-                        print('OsClass.osgen : {0}'.format(osclass['osgen']))
-                        print('OsClass.accuracy : {0}'.format(osclass['accuracy']))
-                        print('')
+    results = []
+    for ip in PeckerArgs.ips:
+        open_ports = scan_ports(ip)
+        os_identification = scan_os(ip)
+        dns_server = PeckerArgs.dns
+        hostname = resolve_hostname(ip, dns_server)
+        result = {'IP': ip, 'Open Ports': open_ports, 'OS Identification': os_identification, 'Hostname': hostname}
+        results.append(result)
 
-    for proto in Pecker[host].all_protocols():
-        print('----------')
-        print('Protocol: %s' % proto)
-
-        lport = Pecker[host][proto].keys()
-        for port in lport:
-            print('port: %s\tstate : %s' % (port, Pecker[host][proto][port]['state']))
-        #print('OS: %s' % OsDetector[host]['osmatch'][0]['osclass'][0]['osfamily'])
-    print(Pecker.csv())
-    #print(OsDetector.csv())
+    with open(PeckerArgs.output, mode='w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=['IP', 'Open Ports', 'OS Identification', 'Hostname'], delimiter='\t')
+        writer.writeheader()
+        for result in results:
+            writer.writerow(result)
 
 if __name__ == "__main__":
     main()
